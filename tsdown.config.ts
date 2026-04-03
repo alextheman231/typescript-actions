2af08520b5b4aeef95235f718d0fb7038dd08832
AlexMan123456 <alex.man03888@gmail.com>
2026-04-03 01:05:42 +0100

    The internal entrypoint of utility needs bundling

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,11 @@
 import type { UserConfig } from "tsdown";
 import packageInfo from "./package.json" with { type: "json" };
 
+const ALL_THE_DEPENDENCIES_TO_BUNDLE_BECAUSE_GITHUB_ACTIONS_IS_GARBAGE = [
+  ...Object.keys(packageInfo.dependencies),
+  "@alextheman/utility/internal",
+];
+
 const config: Array<UserConfig> = [
   {
     entry: ["src/safe-npm-dependency-global-install/index.ts"],
@@ -10,7 +15,7 @@ const config: Array<UserConfig> = [
     clean: true,
     fixedExtension: false,
     deps: {
-      alwaysBundle: [...Object.keys(packageInfo.dependencies)],
+      alwaysBundle: ALL_THE_DEPENDENCIES_TO_BUNDLE_BECAUSE_GITHUB_ACTIONS_IS_GARBAGE,
     },
   },
 ];
